3f81b5e5773e594b6db26dbd6fbaee0006d0f8fe
AnandShirbhaiyye <anandshirbhaiyye@gmail.com>
2023-12-22 14:25:56 +0530

    added userdetails get api integration

--- client/src/views/UserDetails/UserDetails.js
+++ client/src/views/UserDetails/UserDetails.js
@@ -1,13 +1,47 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../../components/Navbar/Navbar";
 
 function UserDetails() {
+  const [userdetails, setUserdetails] = useState([]);
+
+  const loadDetails = async () => {
+    try {
+      const response = await axios.get("/api/userdetails");
+      console.log(response?.data?.data);
+      setUserdetails(response?.data?.data);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    loadDetails();
+  }, []);
   return (
     <>
-    <Navbar/>
-    <h1 className='text-center'>UserDetails</h1>
+      <Navbar />
+      <h1 className="text-center">UserDetails</h1>
+      <div className="container">
+        {userdetails?.map((users, index) => {
+          const { name, email, password, mobile, address, gender } = users;
+          return (
+            <div key={index} className="user-card shadow-lg p-5 mb-5 mt-5">
+              <h3>
+                Name:{name} <br />
+                Email: {email} <br />
+                Password: {password}
+                <br />
+                Mobile: {mobile} <br />
+                Address: {address} <br />
+                Gender: {gender}
+              </h3>
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default UserDetails
+export default UserDetails;
